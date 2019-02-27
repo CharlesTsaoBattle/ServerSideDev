77907701f36f271dd384fd2d1265c305464458b7
CharlesTsaoBattle <41309773+CharlesTsaoBattle@users.noreply.github.com>
2019-02-28 10:46:16 +1300

    Deleted commented code

--- django_projects/pineappleexpress/pineapple/models.py
+++ django_projects/pineappleexpress/pineapple/models.py
@@ -13,7 +13,6 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     quantity = models.IntegerField(blank=True)
     datelastorder = models.DateTimeField(blank=True)
-#    image = models.ImageField(blank=True)
 
 
     def __str__(self):
@@ -21,32 +20,3 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[str(self.id)])
-
-
-
-#    CAN ALL THE BELOW BE REMOVED?
-
-# class ProductInstance(models.Model):
-#     """Model representing a specific instance of a product"""
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-#     product_name = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
-#     customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-
-#     class Meta:
-#         ordering = ['product_name']
-        
-#     def __str__(self):
-#         """String for representing the Model object."""
-#         return '{0} ({1})'.format(self.id, self.product.product_name)
-
-
-# class Customer(models.Model):
-
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50)
-#     customer_username = models.CharField(max_length=30)
-#     email_address = models.CharField(max_length=30)
-#     phone_number = models.CharField(max_length=15)
-
-#     def __str__(self):
-#         return self.first_name + ' ' + self.last_name
